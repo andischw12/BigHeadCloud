8666dd1514d18cb81777be24f1f2be21640272ec
andischw12 <andischw12@gmail.com>
2021-09-05 19:45:24 +0300

    Update lookatactivecam.cs

--- Assets/Scripts/lookatactivecam.cs
+++ Assets/Scripts/lookatactivecam.cs
@@ -7,11 +7,7 @@ public class lookatactivecam : MonoBehaviour
     
     private void LateUpdate()
     {
-        if (Cameras.instance == null) // normal follow
-        {
-            this.transform.LookAt(new Vector3(flow_target.transform.position.x, transform.position.y, flow_target.transform.position.z));
-        }
-        if (Cameras.instance != null && Cameras.instance.ActiveCamera != null)// use cameras script
+        if (Cameras.instance.ActiveCamera != null)
         {
             flow_target = Cameras.instance.ActiveCamera.transform;
             this.transform.LookAt(new Vector3(flow_target.transform.position.x, transform.position.y, flow_target.transform.position.z));
