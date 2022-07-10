1ea22334381731c9e9d58e8e02da5343d7a12fb0
andischw12 <89977960+andischw12@users.noreply.github.com>
2022-07-10 21:54:59 +0300

    save

--- Assets/TestManager.cs
+++ Assets/TestManager.cs
@@ -6,19 +6,68 @@ public class TestManager : MonoBehaviour
 {
 
     [SerializeField] int prefabNum;
+    [SerializeField] GameObject AvatarPrefab;
+    public GameObject[] AvatarInstance;
+   [SerializeField] Transform instance0;
+
+    int Hatcounter = 0;
+
+    int Glassescounter = 0;
     // Start is called before the first frame update
     void Start()
     {
-        FindObjectOfType<KidAvatarSelector>().PreperePrefabArr();
-        FindObjectOfType<KidAvatarSelector>().SelectAvatarByPrefab(prefabNum);
+        //AvatarPrefab.GetComponent<KidAvatarSelector>().PreperePrefabArr();
+
+        int arr  = AvatarPrefab.GetComponent<KidAvatarSelector>().PrefabArr.Length;
+        AvatarInstance = new GameObject[arr];
+
+
+        for (int i =0;i<arr;i++)
+        {
+            AvatarInstance[i] = Instantiate(AvatarPrefab,instance0.position + i*Vector3.left,Quaternion.identity);
+            AvatarInstance[i].GetComponent<KidAvatarSelector>().PreperePrefabArr();
+          AvatarInstance[i].GetComponent<KidAvatarSelector>().SelectAvatarByPrefab(i);
+
+          //  AvatarInstance[i].GetComponent<KidAvatarSelector>().SetAvatarDressItem(AvatarArrayEnum.Glasses, 0, 0);
+           // AvatarInstance[i].get
+        }
+        //SetHats();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.H)) 
         {
-            FindObjectOfType<KidAvatarSelector>().SetAvatarAccessoryItem(AvatarArrayEnum.Hats, FindObjectOfType<KidAvatarSelector>().GetActiveAvatarInfo()[AvatarArrayEnum.Hats.GetHashCode()+1]);
+            Hatcounter++;
+            SetHats();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+           Glassescounter++;
+            SetGlasses();
+        }
+
+    }
+
+    void SetHats() 
+    {
+        foreach (GameObject GM in AvatarInstance)
+        {
+            GM.GetComponentInChildren<KidAvatarSelector>().SetAvatarAccessoryItem(AvatarArrayEnum.Hats, Hatcounter);
+
+        }
+    }
+
+
+    void SetGlasses()
+    {
+        foreach (GameObject GM in AvatarInstance)
+        {
+            GM.GetComponentInChildren<KidAvatarSelector>().SetAvatarAccessoryItem(AvatarArrayEnum.Glasses, Glassescounter);
+
         }
     }
 }
