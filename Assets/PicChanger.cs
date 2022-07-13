bb761a3fbf30a7c1282971c3e1a96122248cd1ed
andischw12 <89977960+andischw12@users.noreply.github.com>
2022-07-13 20:57:32 +0300

    save

--- Assets/PicChanger.cs
+++ Assets/PicChanger.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PicChanger : MonoBehaviour
 {
+
+    [SerializeField] Sprite[] IconsSource;
+    [SerializeField] Image[] TargetArr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Image[] tmpArr = GetComponentsInChildren<Image>();
+
+
+        TargetArr = new Image[IconsSource.Length];
+        for(int i = 0; i < tmpArr.Length; i++) 
+        {
+            if(tmpArr[i].gameObject.transform.name == "Icon") 
+            {
+                print("icon found");
+            }
+        }
+
+
+
     }
 
     // Update is called once per frame
