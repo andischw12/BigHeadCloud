cbb56fdaad8486e7d4db56205b7ad1ad025e0395
andischw12 <89977960+andischw12@users.noreply.github.com>
2022-08-15 15:22:59 +0300

    save

--- Assets/Scripts/FamilyManager.cs
+++ Assets/Scripts/FamilyManager.cs
@@ -263,7 +263,7 @@ public class FamilyManager : MonoBehaviour
 
 #if (!UNITY_EDITOR && !DEVELOPMENT_BUILD)
  
-            _kidsUserArr[i].SetInfoVal(UserArrayEnum.Gems, 300000000);
+            _kidsUserArr[i].SetInfoVal(UserArrayEnum.Gems, 3000);
              _kidsUserArr[i].SetInfoVal(UserArrayEnum.Points, ProfileManager.FIRST_RANK_POINTS);
 #endif
 
