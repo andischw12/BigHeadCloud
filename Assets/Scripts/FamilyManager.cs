1180ac43b72e5e680df63b6e7787289247a72705
andischw12 <89977960+andischw12@users.noreply.github.com>
2022-09-21 18:47:01 +0300

    save

--- Assets/Scripts/FamilyManager.cs
+++ Assets/Scripts/FamilyManager.cs
@@ -259,8 +259,8 @@ public class FamilyManager : MonoBehaviour
         _kidsUserArr[i] = new KidUser(i);
         _kidsUserArr[i].SetInfoVal(UserArrayEnum.Gems, 3000);
         _kidsUserArr[i].SetInfoVal(UserArrayEnum.Points, ProfileManager.FIRST_RANK_POINTS);
-       // return;
-        //_kidsUserArr[i].SetInfoVal(UserArrayEnum.Gems, 300000000);
+        return;
+        _kidsUserArr[i].SetInfoVal(UserArrayEnum.Gems, 300000000);
         _kidsUserArr[i].SetInfoVal(UserArrayEnum.Points, 50000);
 
 
