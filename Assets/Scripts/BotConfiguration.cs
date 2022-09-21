c84788e676c8244e144ed461c6a4928f9970fe01
andischw12 <89977960+andischw12@users.noreply.github.com>
2022-09-21 18:28:16 +0300

    save

--- Assets/Scripts/BotConfiguration.cs
+++ Assets/Scripts/BotConfiguration.cs
@@ -22,6 +22,6 @@ public class BotConfiguration : MonoBehaviour
     public int BotPoints() 
     {
 
-        return ProfileManager.GetPointsByRank(_botRank)/2;
+        return Mathf.Max(ProfileManager.FIRST_RANK_POINTS, ProfileManager.GetPointsByRank(_botRank)/2);
      }
 }
